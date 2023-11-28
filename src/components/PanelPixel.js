f3c9f87dba1525741d633915f1b024f54078bc74
Anuththiga <anuththiga25jt@gmail.com>
2023-11-28 17:13:54 +0800

    pixel art functions

--- src/components/PanelPixel.js
+++ src/components/PanelPixel.js
@@ -3,11 +3,32 @@ import "../styles/panelPixel.scss";
 
 
 const PanelPixel = ({ selectedColor }) => {
-    const [pixelColor, setPixelColor] = useState("#ffff")
-  return (
+    const [pixelColor, setPixelColor] = useState("#ffff");
+    const [oldColor, setOldColor] = useState(pixelColor);
+    const [canChangeColor, setCanChangeColor] = useState(true);
+    
+    const applyColor = () => {
+        setPixelColor(selectedColor);
+        setCanChangeColor(false);
+    }
+
+    const changeColor = () => {
+        setOldColor(pixelColor);
+        setPixelColor(selectedColor)
+    }
+
+    const resetColor = () => {
+        if(canChangeColor) setPixelColor(oldColor);
+
+        setCanChangeColor(true);
+    }
+    return (
     <div
         className="pixel"
         style={{ backgroundColor: pixelColor }}
+        onClick={applyColor}
+        onMouseEnter={changeColor}
+        onMouseLeave={resetColor}
     ></div>
   )
 }
